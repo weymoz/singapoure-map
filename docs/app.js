c107991101cc7899eb5377b2517326c06945d7a8
Denis Grischenko <weymoze@gmail.com>
2019-07-21 01:13:25 +0300

    adds interactive pins

--- docs/app.js
+++ docs/app.js
@@ -9,20 +9,58 @@ function _iterableToArray(iter) { if (Symbol.iterator in Object(iter) || Object.
 function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = new Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } }
 
 (function () {
-  var pins = document.getElementsByClassName('pin');
-  pins = _toConsumableArray(pins);
+  var MAP_CLASS = 'map';
+  var MAP_IMG_CLASS = 'map__img';
+  var POPUP_CLASS = 'popup';
+  var POPUP_SHOW_CLASS = 'popup_show';
+  var PIN_CLASS = 'pin';
+  var PIN_CLICKED_CLASS = 'pin__clicked';
+  var PIN_CLICKED_SHOW_CLASS = 'pin__clicked_SHOW'; //get all pins
+
+  var pins = document.getElementsByClassName(PIN_CLASS); //transform to regular array
+
+  pins = _toConsumableArray(pins); //set click handler for each pin
+
   pins.forEach(function (pin) {
-    return pin.addEventListener('click', pinClickPopupHandler);
-  });
+    pin.addEventListener('click', pinClickHandler);
+  }); //set click handler for the empty space on the map
 
-  function pinClickPopupHandler() {
-    var popup = _toConsumableArray(this.children).reduce(function (acc, elem) {
-      return elem.classList.contains('popup') ? elem : acc;
+  var map = document.getElementsByClassName(MAP_CLASS)[0];
+  map.addEventListener('click', mapClickHandler.bind(null, pins)); //-------------------------------------------
+  // pin functions
+
+  function pinClickHandler() {
+    togglePopup(this);
+    togglePinClicked(this);
+  }
+
+  function findChild(parent, childClass) {
+    return _toConsumableArray(parent.children).reduce(function (acc, elem) {
+      return elem.classList.contains(childClass) ? elem : acc;
     }, null);
+  }
 
-    var pinInner = this.nextElementSibling;
-    console.log(pinInner, popup);
-    popup.classList.toggle('popup_show');
-    pinInner.classList.toggle('pininner_show');
+  function togglePopup(pin) {
+    findChild(pin, POPUP_CLASS).classList.toggle(POPUP_SHOW_CLASS);
+  }
+
+  function togglePinClicked(pin) {
+    findChild(pin, PIN_CLICKED_CLASS).classList.toggle(PIN_CLICKED_SHOW_CLASS);
+  }
+
+  function mapClickHandler(pins, evt) {
+    if (!evt.target.classList.contains(MAP_IMG_CLASS)) return;
+    pins.forEach(function (pin) {
+      hidePopup(pin);
+      hidePinClicked(pin);
+    });
+  }
+
+  function hidePopup(pin) {
+    findChild(pin, POPUP_CLASS).classList.remove(POPUP_SHOW_CLASS);
+  }
+
+  function hidePinClicked(pin) {
+    findChild(pin, PIN_CLICKED_CLASS).classList.remove(PIN_CLICKED_SHOW_CLASS);
   }
 })();
--- src/js/app.js
+++ src/js/app.js
@@ -1,21 +1,76 @@
 (function() {
 
-  let pins = document.getElementsByClassName('pin');
+  const MAP_CLASS = 'map';
+  const MAP_IMG_CLASS = 'map__img';
+  const POPUP_CLASS = 'popup';
+  const POPUP_SHOW_CLASS = 'popup_show';
+  const PIN_CLASS = 'pin';
+  const PIN_CLICKED_CLASS = 'pin__clicked';
+  const PIN_CLICKED_SHOW_CLASS = 'pin__clicked_SHOW';
+
+  //get all pins
+  let pins = document.getElementsByClassName(PIN_CLASS);
+
+  //transform to regular array
   pins = [...pins];
-  
-  pins.forEach(pin => pin.addEventListener('click', pinClickPopupHandler));
-  
-  function pinClickPopupHandler() {
-    const popup = 
-      [...this.children]
-        .reduce((acc, elem) => 
-          elem.classList.contains('popup') ? elem : acc, null);
 
-    const pinInner = this.nextElementSibling;
-    console.log(pinInner, popup);
+  //set click handler for each pin
+  pins.forEach(pin => {
+    pin.addEventListener('click', pinClickHandler);
+  });
 
-    popup.classList.toggle('popup_show');
-    pinInner.classList.toggle('pininner_show');
+  //set click handler for the empty space on the map
+  const map = document.getElementsByClassName(MAP_CLASS)[0];
+  map.addEventListener('click', mapClickHandler.bind(null, pins));
+
+
+  //-------------------------------------------
+  // pin functions
+
+  function pinClickHandler() {
+    togglePopup(this);
+    togglePinClicked(this);
   }
+
+
+  function findChild(parent, childClass) {
+    return [...parent.children].reduce((acc, elem) => 
+    elem.classList.contains(childClass) ? elem : acc, null);
+  }
+
+
+  function togglePopup(pin) {
+    findChild(pin, POPUP_CLASS)
+      .classList.toggle(POPUP_SHOW_CLASS);
+  }
+
+
+  function togglePinClicked(pin) {
+    findChild(pin, PIN_CLICKED_CLASS)
+      .classList.toggle(PIN_CLICKED_SHOW_CLASS);
+  }
+
+
+  function mapClickHandler(pins, evt) {
+    if(!evt.target.classList.contains(MAP_IMG_CLASS)) 
+      return;
+    pins.forEach(pin => {
+      hidePopup(pin);
+      hidePinClicked(pin);
+    });
+  }
+
+
+  function hidePopup(pin) {
+    findChild(pin, POPUP_CLASS)
+      .classList.remove(POPUP_SHOW_CLASS);
+  }
+
+
+  function hidePinClicked(pin) {
+    findChild(pin, PIN_CLICKED_CLASS)
+      .classList.remove(PIN_CLICKED_SHOW_CLASS);
+  }
+
 
 })();
